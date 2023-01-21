504df1e8517bfea32e6060433435c60503e56e50
ImTaeuk <100995244+ImTaeuk@users.noreply.github.com>
2023-01-21 15:38:00 +0900

    Revert "Update assignment"

--- app.py
+++ app.py
@@ -4,16 +4,11 @@ app = Flask(__name__)
 
 @app.route('/id/<int:id>', methods=['GET'])
 def get(id):
-    if id >= 5000:
-        return  { 'result' : True }
-    else:
-        return { 'result' : False }
-
+    return {"message": ""}
 
 @app.route('/id', methods=['POST'])
 def post():
-    params = request.get_json()
-    return {'name': params['name'] }
+    return {'name': ''}
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
